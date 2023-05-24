71319a8e028ba5dc4ce083c41a9fd0594274c003
Kristjan Pekk <kristjan.pekk@cyber.ee>
2023-05-24 18:27:31 +0300

    %1

--- scripts/git_publish.sh
+++ scripts/git_publish.sh
@@ -1,18 +1,14 @@
 @echo off
 
-rem Step 1: npm run build
 echo Running 'npm run build'...
 npm run build
 
-rem Step 2: git add .
 echo Running 'git add .'...
 git add .
 
-rem Step 3: git commit -m "%1"
 echo Running 'git commit'...
 git commit -m "%1"
 
-rem Step 4: git push
 echo Running 'git push'...
 git push
 
