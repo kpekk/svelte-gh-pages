8e61af1221f24bf02ed31a3dd4191d43a5ed7548
Kristjan Pekk <kristjan.pekk@gmail.com>
2023-05-27 22:47:42 +0300

    cloud component hover effect completed

--- src/Cloud.svelte
+++ src/Cloud.svelte
@@ -1,114 +1,99 @@
 <script>
-    let title = "DnD";
-    let description = "example description mlem";
-    let size = "5";
-    let showInfo = false;
-  
-    const showInfoPanel = () => {
-      showInfo = true;
-    };
-  
-    const hideInfoPanel = () => {
-      showInfo = false;
-    };
-  
-    $: {
-      // @ts-ignore
-      document.querySelector(":root").style.setProperty("--size", size + "rem");
-    }
-  </script>
-  
-  <!-- svelte-ignore a11y-mouse-events-have-key-events -->
-  <div class="cloud">
-    <div class="text-container">
-      <div>{title}</div>
-    </div>
-    <svg viewBox="15 -10 15 42">
-      <path
-        d="M 0,6 
+  let title = "DnD";
+  let description = "example description mlem";
+  let size = "5";
+  let hovered = false;
+
+  const showInfoPanel = () => {
+    hovered = true;
+  };
+
+  const hideInfoPanel = () => {
+    hovered = false;
+  };
+
+  $: {
+    // @ts-ignore
+    document.querySelector(":root").style.setProperty("--size", size + "rem");
+  }
+</script>
+
+<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+<div class="cloud">
+  <svg viewBox="15 -10 15 42" class="path-container" class:halo={hovered}>
+    <path
+      d="M 0,6 
            a 10,10 1 0,0 0,25 
            h 45 
            a 10,10 1 0,0 0,-25 
            a 10,10 1 0,0 -15,-5 
            a 15,15 1 0,0 -29, 5  
            z"
-        on:mouseenter={showInfoPanel}
-        on:mouseleave={hideInfoPanel}
-      />
-    </svg>
-    <div class="info" class:show-info={showInfo}>{description}</div>
+      on:mouseenter={showInfoPanel}
+      on:mouseleave={hideInfoPanel}
+    />
+  </svg>
+  <div class="text-container">
+    <div>{title}</div>
   </div>
-  
-  <style>
-    /**https://stackoverflow.com/questions/30919053/creating-a-responsive-cloud-shape*/
-    :root {
-      font-size: 18px;
-      background-color: aqua;
-    }
-  
-    .cloud {
-      height: calc(var(--size) * 2);
-      width: calc(var(--size) * 3.4);
-      position: relative;
-      overflow: visible;
-    }
-  
-    .text-container {
-      position: absolute;
-      top: 45%;
-      left: 0;
-  
-      width: 100%;
-      font-size: 2.7rem;
-  
-      display: flex;
-      align-items: center;
-      justify-content: center;
-  
-      pointer-events: none;
-    }
-  
-    svg {
-      height: 100%;
-      width: 100%;
-    }
-  
-    path {
-      fill: white;
-      stroke: black;
-      stroke-width: 1;
-      stroke-linejoin: round;
-    }
-  
-    .info {
-      position: absolute;
-      left: calc(var(--size) * 0.45);
-      display: none;
-  
-      height: calc(var(--size) * 2);
-      width: calc(var(--size) * 2.5);
-  
-      background-color: rgba(207, 207, 207, 0.1);
-  
-      box-sizing: border-box;
-      border-radius: 1rem;
-      padding: 1rem;
-    }
-  
-    .show-info {
-      display: block;
-    }
-  
-    path:hover {
-      fill: aliceblue;
-      stroke: lightskyblue;
-      filter: drop-shadow(0 0 5px rgba(255, 0, 0, 0.8));
-    }
-  
-    .text-container:hover + svg path {
-      fill: aliceblue;
-      stroke: lightskyblue;
-    }
+  <div class="info" class:show={hovered}>{description}</div>
+</div>
 
-  </style>
-  
+<style>
+  /**https://stackoverflow.com/questions/30919053/creating-a-responsive-cloud-shape*/
+
+  .cloud {
+    height: calc(var(--size) * 2);
+    width: calc(var(--size) * 3.4);
+    position: relative;
+    overflow: visible;
+  }
+
+  svg {
+    height: 100%;
+    width: 100%;
+  }
+
+  path {
+    fill: rgb(247, 254, 255);
+    stroke: rgb(126, 126, 126);
+    stroke-width: 1;
+    stroke-linejoin: round;
+  }
+
+  .text-container {
+    position: absolute;
+    top: 45%;
+    left: 0;
+
+    width: 100%;
+    font-size: 2.7rem;
+
+    display: flex;
+    justify-content: center;
+    pointer-events: none;
+  }
+
+  .info {
+    position: absolute;
+    left: calc(var(--size) * 0.45);
+    display: none;
+
+    height: calc(var(--size) * 2);
+    width: calc(var(--size) * 2.5);
+
+    background-color: rgba(207, 207, 207, 0.1);
+
+    box-sizing: border-box;
+    border-radius: 1rem;
+    padding: 1rem;
+  }
+
+  .show {
+    display: block;
+  }
+
+  .halo {
+    filter: drop-shadow(0 0 1rem rgb(255, 240, 240));
+  }
+</style>
