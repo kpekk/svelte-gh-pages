539bf90f245067fafc21eea7acbf0aa21bb5525a
Kristjan Pekk <kristjan.pekk@gmail.com>
2024-08-17 19:08:50 +0300

    removed unused stuff

--- src/App.svelte
+++ src/App.svelte
@@ -4,21 +4,21 @@
     <meta name="author" content="Kristjan Pekk" />
 
     <link rel="icon" href="../icon.ico" type="image/x-icon" />
-    <link rel="stylesheet" type="text/css" href="../common.css" />
 </svelte:head>
 <script>
-	import Test from "./pages/Test.svelte";
     import Router from 'svelte-spa-router';
 	import Pets from "./pages/Pets.svelte";
     import Kelly from "./pages/Kelly.svelte";
 
     // Define routes
     const routes = {
-        '/': Test,
-		'/another' : Pets,
+		'/pets' : Pets,
         '/kelly' : Kelly
 		};
 </script>
 <Router {routes} />
 
-
+<!-- 
+<a href="#/kelly">Kelly</a>
+<a href="#/pets">Pets</a>
+-->
--- src/pages/Test.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import Pulse from "../components/Pulse.svelte";
-</script>
-
-<h1>totally new page!!!!!!</h1>
-<Pulse color='red'/>
-
-<a href="#/">Home</a>
-<a href="#/another">another</a>
